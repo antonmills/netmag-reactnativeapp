c247393ca469b67928b02e989fb55e1908ba8321
Anton Mills <antonmills1981@googlemail.com>
2015-05-09 12:01:43 +0100

    view items laid out for main view

---
+++ App/Views/WeatherView.js
@@ -0,0 +1,65 @@
+'use strict'
+
+var React = require('react-native')
+
+var {
+	StyleSheet,
+	View,
+	Text,
+	Image
+} = React;
+
+
+
+// constants used for background colors
+var BG_HOT  = "#fbd84d";
+var BG_WARM = "";
+var BG_COLD = "#00abe6";
+
+// to move to API
+var REQUEST_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+var DUMMY_DATA = { "coord":{"lon":151.21,"lat":-33.87},"sys":{"message":0.6,"country":"Australia","sunrise":1430944454,"sunset":1430982554},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],"base":"stations","main":{"temp":284.702,"temp_min":284.702,"temp_max":284.702,"pressure":1026.23,"sea_level":1032.61,"grnd_level":1026.23,"humidity":98},"wind":{"speed":3.15,"deg":247.003},"clouds":{"all":0},"dt":1431034577,"id":2147714,"name":"Sydney","cod":200}
+
+
+
+var WeatherView = React.createClass({
+
+  testMethod: function(st) {
+
+  },
+
+  render: function() {
+    var something;
+    return (
+      <View>
+        <Text>this is the voice of the mysterons</Text>
+      </View>
+    )
+  }
+});
+
+var styles = StyleSheet.create({
+	container: {
+    flex: 1,
+    backgroundColor: BG_HOT,
+  },
+  hamburger: {
+    // position: "absolute",
+    // top: 25,
+    // right: 10,
+    width: 20,
+    height: 15
+  },
+  centreContainer: {
+    // paddingTop: 50,
+    width: 375,
+    height: 500,
+    color: "#FFFFFF",
+  },
+  mainGraphic: {
+    width: 132,
+    height: 132,
+  }
+});
+
+module.exports = WeatherView;
