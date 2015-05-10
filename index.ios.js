96be07785d32afbc713aaa108e299b115dcb00b2
Anton Mills <antonmills1981@googlemail.com>
2015-05-10 23:06:43 +0100

    enemy turns are in.

--- index.ios.js
+++ index.ios.js
@@ -15,17 +15,66 @@ var {
 } = React;
 
 // constants used for background colors
-var BG_HOT  = "#fbd84d";
-var BG_WARM = "";
+var BG_HOT  = "#fb9f4d";
+var BG_WARM = "#fbd84d";
 var BG_COLD = "#00abe6";
 
 // to move to API
-var REQUEST_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
-var DUMMY_DATA = { "coord":{"lon":151.21,"lat":-33.87},"sys":{"message":0.6,"country":"Australia","sunrise":1430944454,"sunset":1430982554},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}],"base":"stations","main":{"temp":284.702,"temp_min":284.702,"temp_max":284.702,"pressure":1026.23,"sea_level":1032.61,"grnd_level":1026.23,"humidity":98},"wind":{"speed":3.15,"deg":247.003},"clouds":{"all":0},"dt":1431034577,"id":2147714,"name":"Sydney","cod":200}
+var REQUEST_URL = "http://api.openweathermap.org/data/2.5/find?units=metric&q=sydney,australia";
+var DUMMY_DATA = {"message":"accurate","cod":"200","count":1,"list":[{"id":2147714,"name":"Sydney","coord":{"lon":151.207,"lat":-33.8696},"main":{"temp":12.37,"temp_min":12.37,"temp_max":12.37,"pressure":1021.27,"sea_level":1027.6,"grnd_level":1021.27,"humidity":99},"dt":1431187260,"wind":{"speed":3.16,"deg":305.001},"sys":{"country":"Australia"},"clouds":{"all":0},"weather":[{"id":800,"main":"Clear","description":"Sky is Clear","icon":"01n"}]}]};
 
 
 var reactnativeapp = React.createClass({
+  getInitialState: function() {
+    return {
+      weatherData: null,
+    };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          weatherData: responseData,
+        });
+      })
+      .done();
+  },
+  renderLoadingView: function() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading Data
+        </Text>
+      </View>
+    );
+  },
   render: function() {
+
+    // check if weather data is available
+    // if not, render the loading view
+    if (!this.state.weatherData) {
+      return this.renderLoadingView();
+    }
+
+    // format text used
+    var city = DUMMY_DATA.list[0].name.toUpperCase();
+    var country = DUMMY_DATA.list[0].sys.country.toUpperCase();
+    var temp = parseInt(DUMMY_DATA.list[0].main.temp).toFixed(0);
+
+    // set bg color based on temperature
+    if(temp < 15) {
+      styles.container.backgroundColor = BG_COLD;
+    } else if(temp < 26 && temp > 15) {
+      styles.container.backgroundColor = BG_WARM;
+    } else if(temp > 26) {
+      styles.container.backgroundColor = BG_HOT;
+    }
+
+    // render
     return (
       <View style={styles.container}>
           <View>
@@ -34,13 +83,9 @@ var reactnativeapp = React.createClass({
 
           <View style={styles.centreContainer}>
             <Image source={require('image!weather-sun')} style={styles.weatherIcon} />
-            <Text style={styles.weatherText}>20&deg;</Text>
-            <Text style={styles.weatherTextLight}>{DUMMY_DATA.name},</Text>
-            <Text style={styles.weatherTextLight}>{DUMMY_DATA.sys.country}</Text>
-          </View>
-
-          <View style={styles.copyrightContainer}>
-            <Text style={styles.copyright}>Powered by openweathermap.org</Text>
+            <Text style={styles.weatherText}>{temp}&deg;</Text>
+            <Text style={styles.weatherTextLight}>{city},</Text>
+            <Text style={styles.weatherTextLight}>{country}</Text>
           </View>
       </View>
     );
@@ -50,7 +95,7 @@ var reactnativeapp = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: BG_HOT
+    backgroundColor: BG_COLD
   },
   hamburger: {
     width: 20,
@@ -60,10 +105,9 @@ var styles = StyleSheet.create({
     marginTop: 30,
   },
   centreContainer: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    width: 410,
-    height: 400,
   },
   weatherIcon: {
     width: 132,
@@ -77,15 +121,6 @@ var styles = StyleSheet.create({
   weatherTextLight: {
     fontSize: 32,
     fontWeight: "100",
-    color: "#FFFFFF"
-  },
-  copyrightContainer: {
-    alignSelf: "center",
-    // position: "absolute",
-    // bottom: 20
-  },
-  copyright: {
-    fontSize: 14,
     color: "#FFFFFF"
   }
 });
